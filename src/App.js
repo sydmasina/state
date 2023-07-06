58610484de12ed31732ebc162fc109ee03d91ed3
Sydwell Masina <sydwell.masina@digitaltwin.digital>
2023-07-06 15:28:45 +0200

    Add coinFlipper to main app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import Rando from './Components/Rando/Rando';
-import TestClick from './Components/Click/Click';
-import RandoGame from './Components/RandoGame/RandoGame';
-import Dice from './Components/Dice/Dice';
-import TodoList from './Components/TodoList/TodoList';
+// import Rando from './Components/Rando/Rando';
+// import TestClick from './Components/Click/Click';
+// import RandoGame from './Components/RandoGame/RandoGame';
+// import Dice from './Components/Dice/Dice';
+// import TodoList from './Components/TodoList/TodoList';
+import CoinFlipper from './Components/CoinFlip/CoinFlipper';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TestClick/>
-        <RandoGame/>
-        <Dice />
-        <TodoList />
+        <CoinFlipper />
       </header>
     </div>
   );
