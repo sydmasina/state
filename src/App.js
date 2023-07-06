43af0b61291c87e5faad54bfdb90cbb0d5682ec6
Sydwell Masina <sydwell.masina@digitaltwin.digital>
2023-07-06 08:22:29 +0200

    Everything everywhere all at once!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// import Rando from './Components/Rando/Rando';
-// import TestClick from './Components/Click/Click';
-// import RandoGame from './Components/RandoGame/RandoGame';
-// import Dice from './Components/Dice/Dice';
+import Rando from './Components/Rando/Rando';
+import TestClick from './Components/Click/Click';
+import RandoGame from './Components/RandoGame/RandoGame';
+import Dice from './Components/Dice/Dice';
 import TodoList from './Components/TodoList/TodoList';
 import './App.css';
 
@@ -9,6 +9,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TestClick/>
+        <RandoGame/>
+        <Dice />
         <TodoList />
       </header>
     </div>
